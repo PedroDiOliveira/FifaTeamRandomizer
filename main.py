330e8d2852e19134fe9dcf8f47bab6914d2b4fe9
PedroDiOliveira <126473107+PedroDiOliveira@users.noreply.github.com>
2023-10-09 11:00:56 -0300

    feat: adiciona separar grupos

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import random
 import json
 
+###################################
+##Caminhos para arquivos externos##
+###################################
+##Nao esquecer de colocar 2 barras entre casa caminho para o codigo nao dar erro de leitura
+
+caminhoParticipantes = 'C:\\Users\\Pedro.doliveira\\Desktop\\fifinha\\input\\Participantes.txt'
+caminhoClubes = 'C:\\Users\\Pedro.doliveira\\Desktop\\fifinha\\input\\Clubes.txt'
+caminhoLogs = 'C:\\Users\\Pedro.doliveira\\Desktop\\fifinha\\saves\\logs.txt'
+
+
 ##########################################
 ##Funcao que retorna os nomes dos Clubes##
 ##########################################
 
 def recebeClubes():
 
-    with open('C:\\Users\\pedro\\FIFA\\FifaTeamRandomizer\\input\\Clubes.txt', 'r') as arquivo_times:
+    with open(caminhoClubes, 'r') as arquivo_times:
         linhas = arquivo_times.read().splitlines()
 
     times = []
@@ -24,7 +34,7 @@ def recebeClubes():
 
 def recebeNomes():
 
-    with open('C:\\Users\\pedro\\FIFA\\FifaTeamRandomizer\\input\\Participantes.txt', 'r') as arquivo_nomes:
+    with open(caminhoParticipantes, 'r') as arquivo_nomes:
         linhas = arquivo_nomes.read().splitlines()
 
     nomes = []
@@ -60,7 +70,7 @@ def sorteio():
 #########################################################
 
 def salvaResultados(jogadores, times):#TODO
-    with open('C:\\Users\\pedro\\FIFA\\FifaTeamRandomizer\\saves\\logs.txt', 'w') as arquivo:
+    with open(caminhoLogs, 'w') as arquivo:
         print(X)    
 
 ################################################
@@ -99,10 +109,60 @@ def separaPotes():
     pote4.append(nomes[14])
     pote4.append(nomes[15])
 
-    print(f" Pote 1: {pote1} \n Pote 2: {pote2} \n Pote 3: {pote3} \n Pote 4: {pote4}")
+    #print(f" Pote 1: {pote1} \n Pote 2: {pote2} \n Pote 3: {pote3} \n Pote 4: {pote4}")
+
+    return pote1, pote2, pote3, pote4
+
+###############################
+##Funcao que separa os grupos##
+###############################
+
+def SeparaGrupos():
+    i = 0
+    p1,p2,p3,p4 = separaPotes()
+    grupo1 = []
+    grupo2 = []
+    grupo3 = []
+    grupo4 = []
+    # lista = [grupo1, grupo2, grupo3, grupo4]
+
+    potes = [
+        grupo1,
+        grupo2,
+        grupo3,
+        grupo4,
+    ]    
+    x = 3
+
+    for i in range(4):
+       #1 sorteio
+       indexAleatorio = random.randint(0,x)
+       potes[i].append(p1[indexAleatorio])
+       p1.remove(p1[indexAleatorio])
+
+       #2 sorteio
+       indexAleatorio = random.randint(0,x)
+       potes[i].append(p2[indexAleatorio])
+       p2.remove(p2[indexAleatorio])
+
+       #3 sorteio
+       indexAleatorio = random.randint(0,x)
+       potes[i].append(p3[indexAleatorio])
+       p3.remove(p3[indexAleatorio])
+
+       #4 sorteio
+       indexAleatorio = random.randint(0,x)
+       potes[i].append(p4[indexAleatorio])
+       p4.remove(p4[indexAleatorio])    
+
+       x -= 1
+
     
-
-
+    print(potes[0])
+    print(potes[1])
+    print(potes[2])
+    print(potes[3])
+        
 
 ########################################
 ##Funcao do menu principal de execucao##
@@ -131,43 +191,44 @@ def menu():
         print("")
         opcao = int(input("Digite uma opcao: "))
         if opcao == 1:#Abre o arquivo txt de participantes e mostra todos na tela
-            if verificaArquivoVazio('C:\\Users\\pedro\\FIFA\\FifaTeamRandomizer\\input\\Participantes.txt'):
+            if verificaArquivoVazio(caminhoParticipantes):
                 print("O arquivo parece estar vazio parece estar vazio! Por favor informe os participantes no arquivo 'Participantes.txt'.")
             else:
                 nomes = recebeNomes()
                 print(f'''Participantes:  {nomes}''')
                 continue
         elif opcao == 2:##Abre o arquivo txt de clubes e mostra todos na tela
-            if verificaArquivoVazio('C:\\Users\\pedro\\FIFA\\FifaTeamRandomizer\\input\\Clubes.txt'):
+            if verificaArquivoVazio(caminhoClubes):
                 print("O arquivo parece estar vazio parece estar vazio! Por favor informe os participantes no arquivo 'Clubes.txt'.")
             else:
                 clubes = recebeClubes()
                 print(f'''Participantes:  {clubes}''')
                 continue
         elif opcao == 3:#Verifica se o arquivo de save esta vazio, caso esteja, ele sorteia e manda um novo save!
-            if not verificaArquivoVazio('C:\\Users\\pedro\\FIFA\\FifaTeamRandomizer\\saves\\logs.txt'):
+            if not verificaArquivoVazio(caminhoLogs):
                 sobrepor = int(input("Parece que ja existe um save anterior! Deseja sobrepor o antigo save?(1-sim/2-nao)"))
                 if sobrepor == 1:#caso ja tenha um save ele pergunta ao usuario se quer apagar o save antigo e sorteia novamente
                     resultados = sorteio()
-                    with open('C:\\Users\\pedro\\FIFA\\FifaTeamRandomizer\\saves\\logs.txt', 'w') as arquivo:
+                    with open(caminhoLogs, 'w') as arquivo:
                         arquivo.write('')
-                    with open('C:\\Users\\pedro\\FIFA\\FifaTeamRandomizer\\saves\\logs.txt', 'w') as arquivo:
+                    with open(caminhoLogs, 'w') as arquivo:
                         for jogador, timesorteio in resultados.items():
                             arquivo.write(f"Jogador: {jogador.ljust(12)}    Time: {timesorteio}\n")       
                 else:
                     continue
             else:
                 resultados = sorteio()
-                with open('C:\\Users\\pedro\\FIFA\\FifaTeamRandomizer\\saves\\logs.txt', 'w') as arquivo:
+                with open(caminhoLogs, 'w') as arquivo:
                     for jogador, timesorteio in resultados.items():
                         arquivo.write(f"Jogador: {jogador.ljust(12)}    Time: {timesorteio}\n")
         elif opcao == 4:
-            with open('C:\\Users\\pedro\\FIFA\\FifaTeamRandomizer\\input\\Participantes.txt', 'r') as arquivo:
+            with open(caminhoParticipantes, 'r') as arquivo:
                 nomes = arquivo
+                
 
 
     
 
-#menu()
+menu()
 
-separaPotes()
+#SeparaGrupos()
